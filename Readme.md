70caaa05e045873b867bac9276580dba6cc1e81f
tomoima525 <tomoima525@gmail.com>
2018-05-11 15:19:57 -0700

    init

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Rerun Failed Tests fail
+
+Re-running tests from the button at the bottom fails.
+
+## How to reproduce
+- Run unit tests from the nav bar
+- Re-run test from the button below.
+
+![img](img1.png)
+
+- Error message says
+
+```
+0 test classes found in package 'com.tomoima.testapplication'
+
+Process finished with exit code 254
+Empty test suite.
+```
+
+and can not run failed tests.
+ 
---
+++ app/src/test/java/com/tomoima/testapplication/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.tomoima.testapplication
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun multiply_isCorrect() {
+        assertEquals(4, 3 * 3)
+    }
+}
